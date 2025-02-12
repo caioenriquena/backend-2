c9548f8425ac27791d6bc3937255df2da0cf045e
Caio Enrique Neves Araújo <Hell23@academicos.com>
2025-02-11 21:02:06 -0300

    primeira aula

---
+++ demo/src/main/java/com/example/demo/pessoa/DadosCadastroPessoa.java
@@ -0,0 +1,7 @@
+package com.example.demo.pessoa;
+
+public record DadosCadastroPessoa(String nome,
+                                  String email,
+                                  String fone) {
+
+}
